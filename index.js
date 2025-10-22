563bab1fb5ed109a6a5cf0d108fae44d922f50ee
Jyothika2130 <jyothikasujitha2002@gmail.com>
2025-10-22 23:25:00 +0530

    edited

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const ageFun = () => {
   try {
-    if (age.value == "" || height.value == "" || weight.value == "") {
+    if (age.value === "" || height.value === "" || weight.value === "") {
       result.innerHTML = "Enter valid data";
     } else {
       if (age.value < 2 || age.value > 120) {
-        result.innerHTML = "Enter the age in between 2 and 120";
+        result.innerHTML = "Enter the age between 2 and 120";
         return;
       }
+
       if (height.value <= 0 || weight.value <= 0) {
-        result.innerHTML = "Enter valid data to calculate";
+        result.innerHTML = "Enter valid data";
         return;
       }
+
       const heightInMeters = height.value / 100;
       const bmi = Math.floor(weight.value / (heightInMeters * heightInMeters));
-      let category = ""; 
+
+      let category = "";
       if (bmi < 18.5) {
         category = "Underweight";
-        
       } else if (bmi < 24.9) {
         category = "Normal weight";
       } else if (bmi < 29.9) {
@@ -24,16 +26,17 @@ const ageFun = () => {
       } else {
         category = "Obese";
       }
-      result.innerHTML = "your bmi is: " + bmi + " you are in: " + category;
+
+      result.innerHTML = "Your BMI is: " + bmi + " — You are in: " + category;
     }
   } catch (error) {
-    result.innerHTML= "Enter valid data to calculate";
+    result.innerHTML = "Enter valid data";
   }
 };
+
 const resetFun = () => {
   age.value = "";
   height.value = "";
   weight.value = "";
- result.innerHTML = "result will be here..";
+  result.innerHTML = "result will be here..";
 };
- 
